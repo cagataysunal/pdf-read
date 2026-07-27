e0a71cc50e508e72eb196b638a2cf6be554f46bb
Çağatay Sunal <cagataysunal9199@gmail.com>
2026-07-27 17:11:44 +0300

    fix: add AllowAnyHeader() instead of repeating same method

--- backend/Program.cs
+++ backend/Program.cs
@@ -14,7 +14,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowAngularApp", policy =>
     {
         policy.WithOrigins("http://localhost:4200")
-              .AllowAnyMethod()
+              .AllowAnyHeader()
               .AllowAnyMethod();
     }
     );
